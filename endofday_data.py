f15fd333815c8a19523ec1c668e3a7ad85ab67dd
ajakaiye33 <ajakzhedgar@gmail.com>
2021-06-22 07:57:47 -0700

    ymlfile

--- endofday_data.py
+++ endofday_data.py
@@ -10,26 +10,25 @@ import configparser
 import pandas as pd
 
 
-URL = 'https://doclib.ngxgroup.com/REST/api/statistics/equities/?market=&sector=&orderby=&pageSize=300&pageNo=0'
-
 # save the days trading stats
 
 
 def json2csv(URL=URL):
-    todays = datetime.now()
-    # present = todays.strftime('%d%m%Y')
-    # current = f"./docs/{present}.csv"
+    """
+    Exctract and update csv
+    """
+    URL = str(URL)
     eod = './docs/eodfrm210621.csv'
     df = pd.read_json(URL)
     df = df.dropna()
     df.to_csv(eod, mode='a', header=False, index=False)
 
     # load the aws_boto credentials
-    parser = configparser.ConfigParser()
-    parser.read("eod.conf")
-    AWS_SECRET_ACCESS_KEY = parser.get("aws_cred", "AWS_SECRET_ACCESS_KEY")
-    AWS_ACCESS_KEY_ID = parser.get("aws_cred", "AWS_ACC_KEY_ID")
-    AWS_S3_BUCKET = parser.get("aws_cred", "AWS_S3_BUCKET")
+    # parser = configparser.ConfigParser()
+    # parser.read("eod.conf")
+    # AWS_SECRET_ACCESS_KEY = parser.get("aws_cred", "AWS_SECRET_ACCESS_KEY")
+    # AWS_ACCESS_KEY_ID = parser.get("aws_cred", "AWS_ACC_KEY_ID")
+    # AWS_S3_BUCKET = parser.get("aws_cred", "AWS_S3_BUCKET")
 
     s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
